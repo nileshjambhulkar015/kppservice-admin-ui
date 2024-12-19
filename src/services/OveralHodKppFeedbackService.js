9c5a3ea3a83f378580a90f6dfd42c2452a64028b
NileshDada <nileshjambhulkar@gmail.com>
2024-12-19 19:44:20 +0530

    language added

---
+++ src/services/OveralHodKppFeedbackService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import Cookies from 'js-cookie';
+import { BASE_URL_API, LOGIN_UI_BASE_URL } from "./URLConstants";
+
+
+
+class OveralHodKppFeedbackService {
+
+    getEmployeeDetailsByPagination(data) {
+        if (null != Cookies.get('empId')) {
+            return axios.get(BASE_URL_API + `/employee/employee-kpp-status?empKppStatus=In-Progress&roleId=2&page=${data.currentPage - 1}&size=${data.itemsPerPage}`)
+        } else {
+            alert("You need to login first")
+            window.location.replace(LOGIN_UI_BASE_URL);
+        }
+    }
+
+}
+
+
+export default new OveralHodKppFeedbackService();
